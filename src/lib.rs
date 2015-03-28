71d02e1ac15cd64861b028196627b443bfd56b32
Carol Nichols <carol.nichols@gmail.com>
2015-03-28 17:22:03 -0400

    Add a failing test for four
    $ cargo test
       Compiling prime_factors v0.0.1
    (file:///Users/carolnichols/Rust/prime_factors)
         Running target/prime_factors-2690254f285d4686
    
    running 3 tests
    test prime_factors_of_four ... FAILED
    test prime_factors_of_three ... ok
    test prime_factors_of_two ... ok
    
    failures:
    
    ---- prime_factors_of_four stdout ----
    	thread 'prime_factors_of_four' panicked at 'assertion failed:
    `(left == right) && (right == left)` (left: `[4]`, right: `[2, 2]`)',
    src/lib.rs:18
    
    failures:
        prime_factors_of_four
    
    test result: FAILED. 2 passed; 1 failed; 0 ignored; 0 measured
    
    thread '<main>' panicked at 'Some tests failed',
    /Users/carolnichols/Rust/rust/src/libtest/lib.rs:262

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,8 @@ fn prime_factors_of_two() {
 fn prime_factors_of_three() {
     assert_eq!(prime_factors(3), [3]);
 }
+
+#[test]
+fn prime_factors_of_four() {
+    assert_eq!(prime_factors(4), [2, 2]);
+}
