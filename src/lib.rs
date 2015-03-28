fae610f4a1beb0dd00c7742a230b1a60506aaf22
Carol Nichols <carol.nichols@gmail.com>
2015-03-28 17:18:38 -0400

    Add a failing test for three
    $ cargo test
       Compiling prime_factors v0.0.1
    (file:///Users/carolnichols/Rust/prime_factors)
    src/lib.rs:2:18: 2:21 warning: unused variable: `num`,
     #[warn(unused_variables)] on by default
    src/lib.rs:2 fn prime_factors(num: i64) -> Vec<i64> {
                                  ^~~
    src/lib.rs:2:18: 2:21 warning: unused variable: `num`,
     #[warn(unused_variables)] on by default
    src/lib.rs:2 fn prime_factors(num: i64) -> Vec<i64> {
                                  ^~~
         Running target/prime_factors-2690254f285d4686
    
    running 2 tests
    test prime_factors_of_three ... FAILED
    test prime_factors_of_two ... ok
    
    failures:
    
    ---- prime_factors_of_three stdout ----
    	thread 'prime_factors_of_three' panicked at 'assertion failed:
    `(left == right) && (right == left)` (left: `[2]`, right: `[3]`)',
    src/lib.rs:13
    
    failures:
        prime_factors_of_three
    
    test result: FAILED. 1 passed; 1 failed; 0 ignored; 0 measured
    
    thread '<main>' panicked at 'Some tests failed',
    /Users/carolnichols/Rust/rust/src/libtest/lib.rs:262est for three

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,8 @@ fn prime_factors(num: i64) -> Vec<i64> {
 fn prime_factors_of_two() {
     assert_eq!(prime_factors(2), [2]);
 }
+
+#[test]
+fn prime_factors_of_three() {
+    assert_eq!(prime_factors(3), [3]);
+}
